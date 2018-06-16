4d1e84bb26f258a22ee73956cdc4f5a5638c198b
Trinity Pointard <trinity.pointard@insa-rennes.fr>
2018-06-16 14:28:22 +0200

    Adding of login capability and fix some bugs

--- modules/network.py
+++ modules/network.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from typing import Iterable, Dict, Set, List, Tuple
-from datetime import datetime
+from time import time
 from subprocess import run, PIPE, TimeoutExpired
 import re
 
@@ -26,6 +26,8 @@ class InvalidMacError(NetworkError):
     '''the mac address provided was invalid'''
 class NotInSetError(NetworkError):
     '''can't query or delete this mac, not in set'''
+class FeatureDisabledError(NetworkError):
+    '''a feature required for this method is disabled'''
 class Network:
     name: str
     counter: bool
@@ -33,14 +35,26 @@ class Network:
     markStart: int
     markMod: int
     nextAdd: int
-    def __init__(self, name="langate", defaultTimeout=0, counter=True, marking=True, mark=(0,1)):
+    multiVpnMark: int
+    userLogs: List[Tuple[time, Dict[str, int]]]
+    vpnLogs: List[Tuple[time, Dict[int, int]]]
+    lastUserMeasure: Dict[str, int]
+    lastVpnMeasure: Dict[int, int]
+    def __init__(self, name="langate", defaultTimeout=0, counter=True, marking=True, mark=(0,1), multiVpnMark=4294967295):
+        #CMD=sudo ipset create langate hash:mac -exist hashsize 4096 timeout 0 counters skbinfo
         self.name = name
         self.timeout = defaultTimeout
         self.counter = counter
         self.skbinfo = marking
         (self.markStart,self.markMod) = mark
         self.nextAdd = 0
-        #CMD=sudo ipset create langate hash:mac -exist hashsize 4096 timeout 0 counters skbinfo
+        self.multiVpnMark = multiVpnMark
+        if self.counter:
+            self.userLogs = list()
+            self.lastUserMeasure = dict()
+            if self.skbinfo:
+                self.vpnLogs = list()
+                self.lastVpnMeasure = dict()
         creationArgs = ["sudo", "ipset", "create", self.name, "hash:mac", "-exist", "hashsize", "4096", "timeout", str(defaultTimeout)]
         if self.counter:
             creationArgs.append("counters")
@@ -63,19 +77,31 @@ class Network:
     def verifyMac(mac: str):
         return re.match(r'([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}', mac)
 
-    def connectUser(self, mac: str, timeout=-1):
+    def connectUser(self, mac: str, timeout=None, mark=None,counter=0, multiVpn=False):
         #CMD=sudo ipset add langate 00:00:00:00:00:01 -exist timeout 0 skbmark 0
         if not Network.verifyMac(mac):
             raise InvalidMacError("'{}' is not a valid mac address".format(mac))
 
         connectArgs = ["sudo", "ipset", "add", self.name, mac, "-exist"]
-        if timeout!=-1:
+        if timeout!=None:
             connectArgs.append("timeout")
             connectArgs.append(str(timeout))
+        if self.counter:
+            connectArgs.append("bytes")
+            connectArgs.append(str(counter))
+        elif counter!=0:
+            raise FeatureDisabledError("Feature counter is disables for this set")
         if self.skbinfo:
             connectArgs.append("skbmark")
-            connectArgs.append(str(hex(self.nextAdd+self.markStart)))
-            self.nextAdd = (self.nextAdd+1) % self.markMod:
+            if multiVpn:
+                connectArgs.append(hex(self.statelessMark))
+            elif mark==None:
+                connectArgs.append(hex(self.nextAdd+self.markStart))
+                self.nextAdd = (self.nextAdd+1) % self.markMod
+            else:
+                connectArgs.append(hex(mark))
+        elif mark!=None:
+            raise FeatureDisabledError("Feature skbinfo is disables for this set")
 
         result = run(connectArgs, stderr=PIPE, timeout=2)
 
@@ -99,7 +125,10 @@ class Network:
         if result.returncode!=0:
             raise GenericNetworkError(result.stderr.decode("UTF-8"))
 
-    def getUserStats(self, mac: str) -> (bool, int):
+    def getUserInfo(self, mac: str) -> (bool, int, int):
+        #CMD=sudo ipset test langate 00:00:00:00:00:01 -q
+        #CMD=sudo ipset list langate | grep 00:00:00:00:00:01
+
         if not Network.verifyMac(mac):
             raise InvalidMacError("'{}' is not a valid mac address".format(mac))
 
@@ -108,40 +137,63 @@ class Network:
         result = run(testArgs, timeout=2)
 
         if result.returncode!=0:
-            return (False, 0)
-        if not self.counter:
-            return (True, 0)
+            return (False, 0, 0)
+        if not self.counter and not self.skbmark:
+            return (True, 0, 0)
 
         listArgs = ["sudo", "ipset", "list", self.name]
-        result = run(listArgs, stdout=PIPE, timeout=2)
+        result = run(listArgs, stdout=PIPE, stderr=PIPE, timeout=2)
+        if result.returncode!=0:
+            raise GenericNetwokError(result.stderr.decode("UTF-8"))
         out = result.stdout.decode("UTF-8")
         for line in out.splitlines():
             if re.match(mac.upper()+'.*', line):
-                byte = re.search('bytes ([0-9]+)', line).group(1)
-                return (True, int(byte))
+                byte = re.search('bytes ([0-9]+)', line)
+                if byte:
+                    byte = int(byte.group(1))
+                else:
+                    byte=0
+                skbmark = re.search('skbmark (0x[0-9]+)', line)
+                if skbmark:
+                    skbmark = int(skbmark.group(1),16)
+                else:
+                    skbmark = 0
+                return (True, byte, skbmark)
+
 
     def clear(self):
+        #CMD=sudo ipset flush langate
         clearArgs = ["sudo", "ipset", "flush", self.name]
         result = run(clearArgs, stderr=PIPE, timeout=2)
         if result.returncode!=0:
-            raise GenericNetwokError(result.stde.decode("UTF-8"))
+            raise GenericNetwokError(result.stderr.decode("UTF-8"))
 
-    def getAllConnected(self) -> Dict[str, int]:
+    def getAllConnected(self) -> Dict[str, Tuple[int,int]]:
+        #CMD=sudo ipset list langate
         listArgs = ["sudo", "ipset", "list", self.name]
-        result = run(listArgs, stdout=PIPE, timeout=2)
+        result = run(listArgs, stdout=PIPE, stderr=PIPE, timeout=2)
+        if result.returncode!=0:
+            raise GenericNetwokError(result.stderr.decode("UTF-8"))
         out = result.stdout.decode("UTF-8")
         res = dict()
         for line in out.splitlines():
             if re.match('([0-9A-F]{2}:){5}[0-9A−F]{2}.*', line):
                 mac = re.search("(([0-9A-F]{2}:){5}[0-9A−F]{2})", line).group(1)
-                if self.counter:
-                    byte = re.search('bytes ([0-9]+)', line).group(1)
+                byte = re.search('bytes ([0-9]+)', line)
+                if byte:
+                    byte = int(byte.group(1))
                 else:
-                    byte = 0
-                res[mac] = byte
+                    byte=0
+                skbmark = re.search('skbmark (0x[0-9]+)', line)
+                if skbmark:
+                    skbmark = int(skbmark.group(1),16)
+                else:
+                    skbmark = 0
+                res[mac] = (byte, skbmark)
         return res
 
     def delete(self):
+        #CMD=sudo ipset destroy langate
         clearArgs = ["sudo", "ipset", "destroy", self.name]
         result = run(clearArgs, stderr=PIPE, timeout=2)
         if result.returncode!=0:
@@ -149,25 +201,100 @@ class Network:
         del self
 
     def logStatistics(self):
+        #CMD=sudo ipset list langate
+
+        if not self.counter:
+            raise FeatureDisabledError("Feature counter is disables for this set")
+
+        listArgs = ["sudo", "ipset", "list", self.name]
+        result = run(listArgs, stdout=PIPE, stderr=PIPE, timeout=2)
+        if result.returncode!=0:
+            raise GenericNetwokError(result.stderr.decode("UTF-8"))
+        out = result.stdout.decode("UTF-8")
+
+        currentTime = time()
+        userLog = dict()
+        if self.skbinfo:
+            vpnLog = dict()
+
+        for line in out.splitlines():
+            if re.match('([0-9A-F]{2}:){5}[0-9A−F]{2}.*', line):
+                mac = re.search("(([0-9A-F]{2}:){5}[0-9A−F]{2})", line).group(1)
+                byte = int(re.search('bytes ([0-9]+)', line).group(1))
+
+                if mac in self.lastUserMeasure:
+                    userLog[mac]=byte - self.lastUserMeasure[mac]
+                else:
+                    userLog[mac]=byte
+                self.lastUserMeasure[mac]=byte
+                if self.skbinfo:
+                    vpn = re.search('skbmark (0x[0-9]+)', line)
+                    if vpn:
+                        vpn=int(vpn.group(1),16)
+                    else:
+                        continue
+                    if vpn in vpnLog:
+                        vpnLog[vpn]+=byte
+                    else:
+                        vpnLog[vpn]= byte
+
+        self.userLogs.append((currentTime, userLog))
+
+        if self.skbinfo:
+            for vpn in vpnLog:
+                if not vpn in self.lastVpnMeasure:
+                    self.lastVpnMeasure[vpn] = 0
+                vpnLog[vpn]-=self.lastVpnMeasure[vpn]
+                self.lastVpnMeasure[vpn]+= vpnLog[vpn]
+            self.vpnLogs.append((currentTime, vpnLog))
+
+
+    def getUsersLogs(self) -> List[Tuple[time, Dict[str, int]]]:
+        if not self.counter:
+            raise FeatureDisabledError("Feature counter is disables for this set")
+        return self.userLogs
+
+    def getVpnLogs(self) -> List[Tuple[time, Dict[int, int]]]:
+        if not self.counter:
+            raise FeatureDisabledError("Feature counter is disables for this set")
+        if not self.skbinfo:
+            raise FeatureDisabledError("Feature skbinfo is disables for this set")
+        return self.vpnLogs
+
+    def clearLogs(self, after=time()):
+        if not self.counter:
+            raise FeatureDisabledError("Feature counter is disables for this set")
+        self.userLogs = list()
+        if self.skbinfo:
+            self.vpnLogs = list()
+
+    def tryBalance(self):
         NotImplemented
 
-    def getUsersLogs(self) -> List[Tuple[datetime, Dict[str, int]]]:
-        NotImplemented
+    def getBalance(self) -> Dict[int, Set[str]]:
+        res = dict()
+        connected = self.getAllConnected()
+        for mac in connected:
+            if connected[mac][1] in res:
+                res[connected[mac][1]].add(mac)
+            else:
+                res[connected[mac][1]] = set()
+                res[connected[mac][1]].add(mac)
+        return res
 
-    def getVpnLogs(self) -> List[Tuple[datetime, Dict[str, int]]]:
-        NotImplemented
+    def setVpn(self,mac: str, vpn: int):
+        if not Network.verifyMac(mac):
+            raise InvalidMacError("'{}' is not a valid mac address".format(mac))
+        if not self.skbinfo:
+            raise FeatureDisabledError("Feature skbinfo is disables for this set")
 
-    def clearLogs(self, after=datetime.now()):
-        NotImplemented
-
-    def tryBalance(self) -> Dict[int, Set[str]]:
-        NotImplemented
-
-    def getBalance(self, vpnid = -1) -> Dict[int, Set[str]]:
-        NotImplemented
-
-    def getVpn(mac: str) -> int:
-        NotImplemented
-
-    def setVpn(mac: str, vpn: int):
-        NotImplemented
+        userInfo = self.getUserInfo(mac)
+        if userInfo[0]:
+            oldVpn = userInfo[2]
+            byte = userInfo[1]
+            self.connectUser(mac, mark=vpn, counter=byte)
+            if self.counter:
+                self.lastVpnMeasure[oldVpn] = self.lastVpnMeasure.get(oldVpn,0) - self.lastUserMeasure.get(mac,0)
+                self.lastVpnMeasure[vpn] = self.lastVpnMeasure.get(vpn,0) + self.lastUserMeasure.get(mac,0)
+        else:
+            raise NotInSetError("'{}' is not in the set")
